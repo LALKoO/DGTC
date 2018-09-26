4510e7c9ac0d75e0d8e1d6fe648c616cb197eaf2
LALKoO <mminem12@gmail.com>
2018-09-26 20:27:06 +0300

    1

--- Assets/Scripts/CamMove.cs
+++ Assets/Scripts/CamMove.cs
@@ -23,11 +23,11 @@ public class CamMove : MonoBehaviour {
         if( a == 1 || a == 5)
             transform.position = new Vector3(player.transform.position.x, player.transform.position.y, -10f);
         else if (a == 2)
-            transform.position = new Vector3(player.transform.position.x, -10f, player.transform.position.z);
+            transform.position = new Vector3(player.transform.position.x, 10f, player.transform.position.z);
         else if (a == 3)
             transform.position = new Vector3(player.transform.position.x, player.transform.position.y, 10f);
         else if (a == 4 || a == 0)
-            transform.position = new Vector3(player.transform.position.x, 10f, player.transform.position.z);
+            transform.position = new Vector3(player.transform.position.x, -10f, player.transform.position.z);
 
         if (a == 0)
             a = 4;
