8511e2e7d9e58f2bc99797a093f88e422f313f17
LALKoO <mminem12@gmail.com>
2018-09-26 20:32:32 +0300

    1

--- Assets/Scripts/CamMove.cs
+++ Assets/Scripts/CamMove.cs
@@ -11,23 +11,23 @@ public class CamMove : MonoBehaviour {
 
         if (Input.GetKeyDown(KeyCode.Q))
         {
-            a++;
+            a--;
             transform.Rotate(0, 90, 0);
         }
         else if (Input.GetKeyDown(KeyCode.E))
         {
-            a--;
+            a++;
             transform.Rotate(0, -90, 0);
         }
 
         if( a == 1 || a == 5)
             transform.position = new Vector3(player.transform.position.x, player.transform.position.y, -10f);
         else if (a == 2)
-            transform.position = new Vector3(player.transform.position.x, 10f, player.transform.position.z);
+            transform.position = new Vector3(10f, player.transform.position.y, player.transform.position.z);
         else if (a == 3)
             transform.position = new Vector3(player.transform.position.x, player.transform.position.y, 10f);
         else if (a == 4 || a == 0)
-            transform.position = new Vector3(player.transform.position.x, -10f, player.transform.position.z);
+            transform.position = new Vector3(-10f, player.transform.position.y, player.transform.position.z);
 
         if (a == 0)
             a = 4;
