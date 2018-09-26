e1e4ff4ad8ce7071cf60c79eefc0e2103d6c7a39
LALKoO <mminem12@gmail.com>
2018-09-26 20:24:27 +0300

    1

--- Assets/Scripts/CamMove.cs
+++ Assets/Scripts/CamMove.cs
@@ -3,16 +3,38 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CamMove : MonoBehaviour {
-
+    public int a = 1;
     public GameObject player;
 
-	void Update () {
-        transform.position = new Vector3(player.transform.position.x, player.transform.position.y, -10f);
-        /*
+	void Update ()
+    {
+
         if (Input.GetKeyDown(KeyCode.Q))
+        {
+            a++;
             transform.Rotate(0, 90, 0);
+        }
         else if (Input.GetKeyDown(KeyCode.E))
+        {
+            a--;
             transform.Rotate(0, -90, 0);
-        */
+        }
+
+        if( a == 1 || a == 5)
+            transform.position = new Vector3(player.transform.position.x, player.transform.position.y, -10f);
+        else if (a == 2)
+            transform.position = new Vector3(player.transform.position.x, -10f, player.transform.position.z);
+        else if (a == 3)
+            transform.position = new Vector3(player.transform.position.x, player.transform.position.y, 10f);
+        else if (a == 4 || a == 0)
+            transform.position = new Vector3(player.transform.position.x, 10f, player.transform.position.z);
+
+        if (a == 0)
+            a = 4;
+        else if (a == 5)
+            a = 1;
+        
+        
+
     }
 }
