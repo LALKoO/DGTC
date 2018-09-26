a4d4cd3fbff0a0b2e424183c3fa1d1985d3b8eae
LALKoO <mminem12@gmail.com>
2018-09-26 20:42:40 +0300

    1

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -14,33 +14,10 @@ public class PlayerControl : MonoBehaviour {
 	}
 
 	void Update () {
-        /*
+
         speed = 1f;
         if (Input.GetKey(KeyCode.LeftShift))
             speed = 6f;
-
-        if (Input.GetKeyDown(KeyCode.Q))
-            a = 1;
-        else if (Input.GetKeyDown(KeyCode.E))
-            a = 0;
-
-
-        if (a == 0)
-        {
-            float moveY = Input.GetAxis("Horizontal");
-           rb.MovePosition(rb.position + Vector3.forward * moveY * speed * Time.deltaTime);
-        }
-        else
-        {
-            float moveX = Input.GetAxis("Horizontal");
-            rb.MovePosition(rb.position + Vector3.right * moveX * speed * Time.deltaTime);
-        }
-        */
-        //if (Input.GetKeyDown(KeyCode.E))
-        //rb.transform.Rotate(0, 90, 0);
-        //else if (Input.GetKeyDown(KeyCode.E))
-        //rb.transform.Rotate(0, -90, 0);
-
 
         if (Input.GetKeyDown(KeyCode.Q))
         {
