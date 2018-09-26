42d5f60d28c538af9adcc559a38f87fb10c8f4b1
LALKoO <mminem12@gmail.com>
2018-09-26 20:41:00 +0300

    1

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -14,6 +14,7 @@ public class PlayerControl : MonoBehaviour {
 	}
 
 	void Update () {
+        /*
         speed = 1f;
         if (Input.GetKey(KeyCode.LeftShift))
             speed = 6f;
@@ -34,13 +35,51 @@ public class PlayerControl : MonoBehaviour {
             float moveX = Input.GetAxis("Horizontal");
             rb.MovePosition(rb.position + Vector3.right * moveX * speed * Time.deltaTime);
         }
+        */
+        //if (Input.GetKeyDown(KeyCode.E))
+        //rb.transform.Rotate(0, 90, 0);
+        //else if (Input.GetKeyDown(KeyCode.E))
+        //rb.transform.Rotate(0, -90, 0);
 
-            //if (Input.GetKeyDown(KeyCode.E))
-            //rb.transform.Rotate(0, 90, 0);
-            //else if (Input.GetKeyDown(KeyCode.E))
-            //rb.transform.Rotate(0, -90, 0);
 
-            if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            a--;
+            transform.Rotate(0, 90, 0);
+        }
+        else if (Input.GetKeyDown(KeyCode.E))
+        {
+            a++;
+            transform.Rotate(0, -90, 0);
+        }
+
+        if (a == 1 || a == 5)
+        {
+            float moveX = Input.GetAxis("Horizontal");
+            rb.MovePosition(rb.position + Vector3.right * moveX * speed * Time.deltaTime);
+        }
+        else if (a == 2)
+        {
+            float moveX = Input.GetAxis("Horizontal");
+            rb.MovePosition(rb.position + Vector3.forward * moveX * speed * Time.deltaTime);
+        }
+        else if (a == 3)
+        {
+            float moveX = Input.GetAxis("Horizontal");
+            rb.MovePosition(rb.position + Vector3.left * moveX * speed * Time.deltaTime);
+        }
+        else if (a == 4 || a == 0)
+        {
+            float moveX = Input.GetAxis("Horizontal");
+            rb.MovePosition(rb.position + Vector3.back * moveX * speed * Time.deltaTime);
+        }
+
+        if (a == 0)
+            a = 4;
+        else if (a == 5)
+            a = 1;
+
+        if (Input.GetKeyDown(KeyCode.Space))
                 rb.AddForce(Vector3.up * 200); 
           
 	}
